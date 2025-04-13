3ec5779d278844cff9422cab87ecdf5fbe5003d2
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-13 23:01:55 +0200

    Update PaymentRepository.java

--- src/main/java/com/example/demo/repositories/PaymentRepository.java
+++ src/main/java/com/example/demo/repositories/PaymentRepository.java
@@ -1,4 +1,15 @@
 package com.example.demo.repositories;
 
-public class PaymentRepository {
+import com.example.demo.models.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findByTripId(Long tripId);
+
+    List<Payment> findByAmountGreaterThan(Double amount);
 }
