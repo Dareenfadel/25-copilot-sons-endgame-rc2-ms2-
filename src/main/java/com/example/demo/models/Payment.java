ebc397cd4f19600ca92cb835094f20ef3d039065
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-13 23:01:58 +0200

    Update Payment.java

--- src/main/java/com/example/demo/models/Payment.java
+++ src/main/java/com/example/demo/models/Payment.java
@@ -1,4 +1,81 @@
 package com.example.demo.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "Payment")
 public class Payment {
+
+    @Id
+    private long id;
+
+    private double amount;
+    private String paymentMethod;
+    private boolean paymentStatus;
+
+    @OneToOne
+    private Trip trip;
+
+    public Payment() {
+    }
+
+    public Payment(double amount, String paymentMethod, Trip trip) {
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = false;
+        this.trip = trip;
+    }
+
+    public Payment(double amount, String paymentMethod, boolean paymentStatus, Trip trip) {
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = paymentStatus;
+        this.trip = trip;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public boolean isPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(boolean paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
