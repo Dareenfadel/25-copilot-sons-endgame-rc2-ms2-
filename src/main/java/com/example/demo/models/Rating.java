14732603e26a0d8fc0dd1b5a0409187c62c0a6d1
Mathew Hany <mathew.hanybb@gmail.com>
2025-04-18 14:26:22 +0200

    feat: Rating

--- src/main/java/com/example/demo/models/Rating.java
+++ src/main/java/com/example/demo/models/Rating.java
@@ -1,4 +1,24 @@
 package com.example.demo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Document(collection = "ratings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rating {
+
+    @Id
+    private String id;
+    private Long entityId;
+    private String entityType;
+    private Integer score;
+    private String comment;
+    private LocalDateTime ratingDate;
 }
