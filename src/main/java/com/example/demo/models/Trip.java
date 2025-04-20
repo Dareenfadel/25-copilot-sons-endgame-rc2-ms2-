c6920fd04f58087614c402dc07442d508945f21e
Darin Mohamed <103316419+Dareenfadel@users.noreply.github.com>
2025-04-20 18:48:34 +0200

    add constructor for trip

--- src/main/java/com/example/demo/models/Trip.java
+++ src/main/java/com/example/demo/models/Trip.java
@@ -52,7 +52,12 @@ public class Trip {
         this.customer = customer;
         this.payment = payment;
     }
-
+    public Trip(LocalDateTime tripDate, String origin, String destination, double tripCost) {
+        this.tripDate = tripDate;
+        this.origin = origin;
+        this.destination = destination;
+        this.tripCost = tripCost;
+    }
     public long getId() {
         return id;
     }
