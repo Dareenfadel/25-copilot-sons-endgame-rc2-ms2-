09a1c6862d61a3ec7e008c0987e48612233eb7e4
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-20 14:53:07 +0200

    add query

--- src/main/java/com/example/demo/repositories/PaymentRepository.java
+++ src/main/java/com/example/demo/repositories/PaymentRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,9 @@ import java.util.List;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    @Query("SELECT p FROM Payment p WHERE p.trip.id = :tripId")
     List<Payment> findByTripId(Long tripId);
 
+    @Query("SELECT p FROM Payment p WHERE p.amount > :amount")
     List<Payment> findByAmountGreaterThan(Double amount);
 }
